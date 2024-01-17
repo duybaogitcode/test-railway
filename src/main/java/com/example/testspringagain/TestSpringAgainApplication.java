1c23d83cb98b91f2ff66ed3a0e5aa8473f4372a6
Duy Bao <bao1322002@gmail.com>
2024-01-17 22:13:34 +0700

    test

---
+++ src/main/java/com/example/testspringagain/TestSpringAgainApplication.java
@@ -0,0 +1,13 @@
+package com.example.testspringagain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestSpringAgainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestSpringAgainApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/testspringagain/TestSpringAgainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testspringagain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestSpringAgainApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
